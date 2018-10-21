95b1fd41532fa27df5c6210cd5df62173a5d4dc1
Arkadii Kudashov <ark100295@yandex.ru>
2018-10-21 18:20:39 +0300

    hw 3-4 column names fix

--- avg_sessions.sql
+++ avg_sessions.sql
@@ -3,4 +3,4 @@ with session_count_per_user as
 select user_id, COUNT(CAST(begin_dttm as DATE)) as sessions from sessions
 group by user_id
 )
-select AVG(IFNULL(sc.sessions, 0)) from users u LEFT JOIN session_count_per_user sc on u.user_id = sc.user_id
+select AVG(IFNULL(sc.sessions, 0)) as avg_sessions_count from users u LEFT JOIN session_count_per_user sc on u.user_id = sc.user_id
